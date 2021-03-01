e4d557d3f664aa179d11d086e2472dfe3462bc7f
Younes Cheikh <younes.cheikh@gmail.com>
2021-03-01 17:28:17 +0100

    Signed assemblies and set plugin info

--- Source/Umator.Plugins.FTP/AssemblyInfo.cs
+++ Source/Umator.Plugins.FTP/AssemblyInfo.cs
@@ -4,6 +4,6 @@
     "8153B5F8-B87C-4958-886D-EBFC081B4370",
     "Umator.FTP",
     "Plugin for communicating with remote host via FTP protocol",
-    Author = "Umator", AuthorUri = "https://umator.com" // ,
-    // DocumentationUri = "https://umator.com/docs/uamtor-email/", SupportUri = "https://umator.com/support/forum/?view=forum&id=13"
+    Author = "Umator", AuthorUri = "https://umator.com" , 
+    DocumentationUri = "https://umator.com/docs/umator-ftp/", SupportUri = "https://umator.com/support/forum/?view=forum&id=17"
 )]
