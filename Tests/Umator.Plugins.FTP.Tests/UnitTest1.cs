73526e2209b3324cfb034856a5dc81a2add97c20
Younes Cheikh <younes.cheikh@gmail.com>
2021-03-01 14:22:26 +0100

    Created Initial project structure

---
+++ Tests/Umator.Plugins.FTP.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Umator.Plugins.FTP.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
